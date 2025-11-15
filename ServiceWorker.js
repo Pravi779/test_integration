f353b27f4d2030ba195540fa5f3491079e22efea
Pravi779 <praveenpravii779@gmail.com>
2025-11-15 16:46:20 +0530

    initial commit

---
+++ ServiceWorker.js
@@ -0,0 +1,4 @@
+	// in your serviceworker.js file
+	// importScripts("<link-that-you-copied>");
+	// for eg, your dashboard host is dashboard-03.moengage.com
+	importScripts("https://cdn.moengage.com/release/dc_3/versions/2/serviceworker_cdn.min.latest.js");
